4e6586c26265f0954363e82a4aa536e4dc427169
arye321 <arye321@users.noreply.github.com>
2023-07-04 15:04:47 +0000

    2

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-from fastapi import FastAPI
-import json 
+from fastapi import FastAPI, HTTPException, Query
+from typing import List
+import json
 app = FastAPI()
 
 data = json.load(open('data.json'))
+tickets = {ticket["id"]: ticket for ticket in data}
 @app.get("/")
 async def root():
     return {"message": "Hello World2"}
-tickets = {ticket["id"]: ticket for ticket in data}
 
 @app.get("/tickets/{ticket_id}")
 def get_ticket(ticket_id: str):
@@ -14,3 +15,33 @@ def get_ticket(ticket_id: str):
         return tickets[ticket_id]
     else:
         return {"error": "Ticket not found"}
+    
+
+@app.get("/search")
+def search_tickets(
+    ticket_id: str = Query(None, title="ID"),
+    title: str = Query(None, title="Title"),
+    content: str = Query(None, title="Content"),
+    email: str = Query(None, title="Email"),
+    created_from: int = Query(None, title="Created From"),
+    created_to: int = Query(None, title="Created To"),
+    labels: List[str] = Query(None, title="Labels"),
+):
+    # Filter the tickets based on the provided parameters
+    results = []
+    for ticket in data:
+        if (
+            (title is None or title.lower() in ticket["title"].lower())
+            and (ticket_id is None or ticket_id.lower() in ticket["id"].lower())
+            and (content is None or content.lower() in ticket["content"].lower())
+            and (email is None or email.lower() in ticket["userEmail"].lower())
+            and (created_from is None or ticket["creationTime"] >= created_from)
+            and (created_to is None or ticket["creationTime"] <= created_to)
+            and (labels is None or all(label in ticket["labels"] for label in labels))
+        ):
+            results.append(ticket)
+
+    if not results:
+        raise HTTPException(status_code=404, detail="No tickets found.")
+
+    return results
